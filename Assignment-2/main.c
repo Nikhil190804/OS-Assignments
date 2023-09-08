82e47638d60920a77e28ab9cf6aa7da836724679
Nikhil <nikhil22322@iiitd.ac.in>
2023-09-08 12:22:45 +0530

    Added code for history

--- Assignment-2/main.c
+++ Assignment-2/main.c
@@ -7,7 +7,18 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
+
+//define the strcut for the command record
+struct Record_of_Command {
+    char command[1000];
+    pid_t pid;
+    time_t startTime;
+    double duration;
+    double memoryUsage;
+};
 
 
 void ctrl_c_pressed(){
@@ -17,12 +28,40 @@ void ctrl_c_pressed(){
 }
 
 
+void add_record_to_history(struct Record_of_Command History[],int *number_of_commands,const char *command, pid_t pid, time_t startTime, double duration,double memoryUsage){
+        struct Record_of_Command *record = &History[*number_of_commands];
+        strncpy(record->command, command, sizeof(record->command));
+        record->pid = pid;
+        record->startTime = startTime;
+        record->duration = duration;
+        record->memoryUsage = memoryUsage;
+        (*number_of_commands)++;
+}
+
+
+void display_records(struct Record_of_Command History[],int number_of_commands){
+    printf("Commands executed are as follows..........");
+    int i=0;
+    while(i<number_of_commands){
+        printf("--------------------");
+        printf("Command: %s\n", History[i].command);
+        printf("PID: %d\n", History[i].pid);
+        printf("Start Time: %s", ctime(&History[i].startTime)); 
+        printf("Duration: %.2f seconds\n", History[i].duration);
+        printf("Memory Usage: %.2f KB\n", History[i].memoryUsage);
+        printf("--------------------");
+        printf("\n");
+    }
+}
+
 
 //main func goes here
 int main(){
+    int number_of_commands=0;
     signal(SIGINT, ctrl_c_pressed);     //process the ctrl+c here 
     bool start_loop=true;
     char input[1000];
+    struct Record_of_Command History[5000];
     printf("Welcome to Custom C Shell.....");
     while(start_loop){
         printf("\n"); 
